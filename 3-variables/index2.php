1b5ac6694b24744d534a40685bde30645bd91507
abdelrhmanhassanmohamed <abdelrhmanhassanmohamed@gmail.com>
2023-07-25 06:39:05 +0300

    up

---
+++ 3-variables/index2.php
@@ -0,0 +1,38 @@
+<!-- problem -->
+<?php
+// $x = 5; 
+// function myTest() {
+//   echo "<p>Variable x inside function is: $x</p>";
+// }
+// myTest();
+// echo "<p>Variable x outside function is: $x</p>";
+?>
+<!-- solution -->
+<?php
+// $x = 5;
+// $y = 10;
+// function myTest() {
+//   global $x, $y;
+//   $y = $x + $y;
+// }
+// myTest();
+// echo $y;
+?>
+<!-- another sol -->
+<?php
+// $x = 5;
+// $y = 10;
+// function myTest() {
+//   $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
+// }
+// myTest();
+// echo $y;
+?>
+<?php
+// function myTest() {
+//   $x = 5;
+//   echo "<p>Variable x inside function is: $x</p>";
+// }
+// myTest();
+// echo "<p>Variable x outside function is: $x</p>";
+?>
