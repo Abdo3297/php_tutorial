a0b841b2ef4c649bae7de5fbf57c15407c7eb188
abdelrhmanhassanmohamed <abdelrhmanhassanmohamed@gmail.com>
2023-07-25 07:02:38 +0300

    update

---
+++ 5-datatype/index.php
@@ -0,0 +1,9 @@
+<?php
+$x1 = "Hello world!"; //string
+$x2 = 5985; //int
+$x3 = 10.365; // float
+$x4 = true; //boolean
+$x5 = array("Volvo","BMW","Toyota"); //array
+// $x6 = new Car("black", "Volvo"); //obj
+$x7 = null; //null
+?>
